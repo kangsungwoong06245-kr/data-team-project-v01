de7723f7dc243987eecb4c1d87d2be4cf91fd0e6
kangsungwoong06245-kr <kangsungwoong06245@gmail.com>
2026-05-14 12:03:23 +0900

    feat: add age group customer analysis query

--- data_analysis.sql
+++ data_analysis.sql
@@ -11,3 +11,16 @@ CREATE TABLE customers (
     );
     
 SELECT * FROM customers;
+
+
+-- A : 연령대별 고객 수 분석 쿼리문
+SELECT
+	CASE
+		WHEN age < 20 THEN '10대 이하',
+        WHEN age < 30 THEN '20대',
+        WHEN age < 40 THEN '30대',
+        ELSE '40eo 이상'
+	END age_group,
+    COUNT(*) customer_count
+FROM customers
+GROUP BY age_group;
